366bceafdee552508ec15bb0a10c487bb4157ccf
Sina Yeganeh <sinayega@gmail.com>
2017-04-24 22:34:45 -0700

    Added radio, sample view

--- c895/FirstViewController.swift
+++ c895/FirstViewController.swift
@@ -11,18 +11,28 @@ import AVKit
 import AVFoundation
 
 class FirstViewController: AVPlayerViewController {
-
+    
+    @IBOutlet var playerView:UIView!
+    var isPlaying:Bool = true;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let playerItem = AVPlayerItem(url: URL(string: "http://www.c895.org/streams/c895sc128.pls")!)
         self.player = AVPlayer(playerItem: playerItem)
         self.player?.play()
+        
         // AVPlayerItem = AVPlayerItem(url: URL(string: "http://www.c895.org/streams/c895sc128.pls")!)
         // player = AVPlayer(playerItem: playerItem)
         
         // To be informed when metadata changes
         // playerItem.addObserver(self, forKeyPath: "timedMetadata", options: [], context: nil)
+    }
+    
+    override func viewWillAppear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+        self.view.addSubview(playerView)
+        
+        // tweak your image frame here as-needed
     }
     
     deinit {
@@ -33,6 +43,12 @@ class FirstViewController: AVPlayerViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playPauseButton(_ sender: UIButton) {
+        print("hit")
+        isPlaying ? self.player?.pause() : self.player?.play()
+        isPlaying = !isPlaying
     }
 
 
