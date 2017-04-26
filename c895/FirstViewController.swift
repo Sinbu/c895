e1293106ae0c0c8111af95f155f9680577c05140
Sina Yeganeh <sinayega@gmail.com>
2017-04-26 02:13:09 -0700

    Added radio player

--- c895/FirstViewController.swift
+++ c895/FirstViewController.swift
@@ -9,48 +9,104 @@
 import UIKit
 import AVKit
 import AVFoundation
+import MediaPlayer
 
-class FirstViewController: AVPlayerViewController {
+class FirstViewController: UIViewController {
     
-    @IBOutlet var playerView:UIView!
+    @IBOutlet var artistAndSongName:UILabel!
+    
     var isPlaying:Bool = true;
+    var songName:String?
+    var artistName:String?
+    var logoImage:UIImage = UIImage(named: "logo2.png")!
+    
+    var player:AVPlayer = AVPlayer()
+    let playerItem = AVPlayerItem(url: URL(string: "http://www.c895.org/streams/c895sc128.pls")!)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let playerItem = AVPlayerItem(url: URL(string: "http://www.c895.org/streams/c895sc128.pls")!)
         self.player = AVPlayer(playerItem: playerItem)
-        self.player?.play()
+        self.player.play()
         
-        // AVPlayerItem = AVPlayerItem(url: URL(string: "http://www.c895.org/streams/c895sc128.pls")!)
-        // player = AVPlayer(playerItem: playerItem)
+        // Get artist/song name
+        playerItem.addObserver(self, forKeyPath: "timedMetadata", options: [], context: nil)
         
-        // To be informed when metadata changes
-        // playerItem.addObserver(self, forKeyPath: "timedMetadata", options: [], context: nil)
+        // Enable Audio session
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, with: .allowAirPlay)
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+                print("AVAudioSession is Active")
+            } catch {
+                print(error)
+            }
+        } catch {
+            print(error)
+        }
+        
+        
+        // Enable Control Center
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.addTarget(handler: { (event) in    // Begin playing the current track
+            self.player.play()
+            return MPRemoteCommandHandlerStatus.success})
+        
+        commandCenter.pauseCommand.addTarget(handler: { (event) in    // Begin playing the current track
+            self.player.pause()
+            return MPRemoteCommandHandlerStatus.success})
+        
     }
     
-    override func viewWillAppear(_ animated:Bool) {
-        super.viewWillAppear(animated)
-        self.view.addSubview(playerView)
-        
-        // tweak your image frame here as-needed
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {        // Get medatata, making sure to support a wider range of characters
+        let origMetaTitle = (playerItem.timedMetadata?.first?.stringValue?.data(using: String.Encoding.isoLatin1, allowLossyConversion: true))!
+        let convertedMetaTitle = String(data: origMetaTitle, encoding: String.Encoding.utf8)!
+        self.artistAndSongName.text = convertedMetaTitle
+        let titleArr = convertedMetaTitle.components(separatedBy: "-")
+        self.artistName = titleArr[0]
+        self.songName = titleArr[1]
+        self.updateNowPlayingInfoCenter()
     }
     
     deinit {
-        // Be a good citizen
-        // playerItem.removeObserver(self, forKeyPath: "timedMetadata")
+        playerItem.removeObserver(self, forKeyPath: "timedMetadata")
     }
-
+    
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewDidAppear(animated)
+        UIApplication.shared.beginReceivingRemoteControlEvents()
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        UIApplication.shared.endReceivingRemoteControlEvents()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // MARK: Helper Functions
+    private func updateNowPlayingInfoCenter() {
+        
+        
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = [
+            MPMediaItemPropertyTitle: self.songName ?? "Unknown Song",
+            MPMediaItemPropertyArtist: self.artistName ?? "Unknown Artist",
+            MPMediaItemPropertyArtwork: MPMediaItemArtwork(image: self.logoImage)
+        ]
+    }
+    
+    // MARK: IBActions
     @IBAction func playPauseButton(_ sender: UIButton) {
-        print("hit")
-        isPlaying ? self.player?.pause() : self.player?.play()
+        isPlaying ? self.player.pause() : self.player.play()
         isPlaying = !isPlaying
     }
-
-
 }
 
